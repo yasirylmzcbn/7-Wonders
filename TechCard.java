a87f05f3517d59c74783636250f3b297d211bb56
RaymondZoo <ray.yijun.zou@gmail.com>
2019-11-04 13:16:31 -0600

    Update TechCard.java

--- TechCard.java
+++ TechCard.java
@@ -4,15 +4,18 @@ public class TechCard extends Card
 {
 	private String techGiven;
 	//constructor
-	public TechCard(String name, String col, ArrayList<String> ch, ArrayList<String> co, int numOfPlayers,String techGiven) {
+	public TechCard(String name, String col, ArrayList<String> ch, ArrayList<String> co, int numOfPlayers,String techGiven) 
+	{
 		super(name, col, ch, co, numOfPlayers);
 		this.techGiven=techGiven;
 	}
 	//getters and setters
-	public String getTechGiven() {
+	public String getTechGiven() 
+	{
 		return techGiven;
 	}
-	public void setTechGiven(String techGiven) {
+	public void setTechGiven(String techGiven) 
+	{
 		this.techGiven = techGiven;
 	}
 	
