24380092ac63fa382ae7bf7750f7fef63e47d8a4
malgantual <56133144+malgantual@users.noreply.github.com>
2019-11-04 13:16:55 -0600

    spacing

--- TechCard.java
+++ TechCard.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class TechCard extends Card
 {
 	private String techGiven;
+	
 	//constructor
 	public TechCard(String name, String col, ArrayList<String> ch, ArrayList<String> co, int numOfPlayers,String techGiven) {
 		super(name, col, ch, co, numOfPlayers);
 		this.techGiven=techGiven;
 	}
+	
 	//getters and setters
 	public String getTechGiven() {
 		return techGiven;
