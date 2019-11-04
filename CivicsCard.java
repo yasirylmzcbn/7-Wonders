eb8a36bc7d07dcb3c340d510b51606815472f46e
nartney <56133153+nartney@users.noreply.github.com>
2019-11-04 13:18:14 -0600

    Update CivicsCard.java

--- CivicsCard.java
+++ CivicsCard.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
 
 public class CivicsCard extends Card
 {
+	private int victoryPoints;
+	
+	//Constructor
+	public CivicsCard(String na, String col, ArrayList<String> ch, ArrayList<String> co, int numOfPlayers,int victoryPoints) {
+		super(na, col, ch, co, numOfPlayers);
+		this.victoryPoints=victoryPoints;
+	}
+	
+	//getters and setters
+	public int getVictoryPoints() {
+		return victoryPoints;
+	}
+	public void setVictoryPoints(int victoryPoints) {
+		this.victoryPoints = victoryPoints;
+	}
 
 }
