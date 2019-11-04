806d06d4fff4e43d8e91a4ff878d00881e2f5c24
malgantual <56133144+malgantual@users.noreply.github.com>
2019-11-04 13:00:34 -0600

    created constructor

--- GuildCard.java
+++ GuildCard.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
 
 public class GuildCard extends Card
 {
-
+	private String id;
+	
+	public GuildCard(String name, String color, ArrayList<String> cost, ArrayList<String> chain, int num, String id)
+	{
+		super(name, color, cost, chain, num);
+	}
 }
