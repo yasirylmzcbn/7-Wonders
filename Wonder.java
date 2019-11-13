7a854970e8e89988df9a55d97ded3f8df6ba4530
Yasir Yilmazcoban <34142336+yasirylmzcbn@users.noreply.github.com>
2019-11-13 13:33:32 -0600

    Update Wonder.java

--- Wonder.java
+++ Wonder.java
@@ -96,8 +96,11 @@ public class Wonder {
 
 	}
 
-	
-	
+	public void playCard(Card c) {
+		if(playable(c))
+			System.out.println("You built "+ c.getName() + ", a " + c.getColor()+" card, by paying " + c.getCost());
+		else System.out.println("You have already built this card");
+	}
 	public void burnCard() {
 		money +=3;
 	}
