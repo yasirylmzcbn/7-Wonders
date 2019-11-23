339deeae2797813e9d219aaff32b4fe24851ef3a
Yasir Yilmazcoban <34142336+yasirylmzcbn@users.noreply.github.com>
2019-11-22 21:56:53 -0600

    Update TextRunner.java

--- TextRunner.java
+++ TextRunner.java
@@ -7,46 +7,40 @@ import java.util.Scanner;
 public class TextRunner
 {
 	public static GameState state;
-	public static Scanner keyboard;
+	public static Scanner input;
 	public static Wonder currentWonder;
 	public static int currentPlayer;
 	public static ArrayList<Card> currentHand;
+	public static boolean endOfRound = false;
 	
 	public static void main(String[] args)
 	{
-		keyboard = new Scanner(System.in);
+		input = new Scanner(System.in);
 		state = new GameState();
-		int testing = 0;
 		while (!state.isEndOfGame())
 		{
+			currentWonder = state.getWonders().get(state.getCurrentPlayer());
+			currentPlayer = state.getCurrentPlayer();
+			currentHand = state.getPlayerHands().get(currentPlayer);
+
 			printNewRound();
-			// for each player in one round
-			while (!state.allDecisionsMade())
-			{
-				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + testing);
-				printDivider();
-				currentWonder = state.getWonders().get(state.getCurrentPlayer());
-				currentPlayer = state.getCurrentPlayer();
-				currentHand = state.getPlayerHands().get(currentPlayer);
-				
-				printPlayerInfo(currentWonder);
-				printOneLine();
-				
-				printPlayedCards();
-				printOneLine();
-				
-				printPlayerHand();
-				printOneLine();
-				
-				optionSelection();
-				printOneLine();
-				
-				state.nextPlayer();
-				//break;
-			}
-			testing += 1;
+			
+			printWonderInformation();
+			printOneLine();
+			
+			printPlayedCards();
+			printOneLine();
+			
+			printPlayerHand();
+			printOneLine();
+			
+			handSelection();
+			printOneLine();
 			
 //			break;
+		
+			state.nextPlayer();
+			
 		}
 	}
 
@@ -90,55 +84,20 @@ public class TextRunner
 		for (int i = 0; i < 25; i++) System.out.print("="); System.out.println();//
 	}
 	
+	public static void printWonderInformation()
+	{
+		System.out.printf("%s; coin:%d, vp:%d, mp:%d, win:%d; loss:%d\n", currentWonder.getName(), currentWonder.getMoney(), currentWonder.getVictoryPoints(),
+				currentWonder.getMilitaryPower(), currentWonder.getWins(), currentWonder.getLosses());
+	}
+	
 	public static void printOneLine()
 	{
 		System.out.println("\n");
 	}
 	
-	public static void printPlayerInfo(Wonder wond)
-	{
-		for (int i = 0; i < 5; i++)
-			System.out.print(">");
-		System.out.printf(" PLAYER %d ::::: %s\tcoins: %d,\tvp: %d,\t mp: %d,\twins: %d,\tlosses: %d\n", state.getCurrentPlayer(), wond.getName(), wond.getMoney(), wond.getVictoryPoints(),
-				wond.getMilitaryPower(), wond.getWins(), wond.getLosses());
-	}
-	
-	public static void printDivider()
-	{
-		for (int i = 0; i < 50; i++)
-			System.out.print("<>");
-		System.out.println();
-	}
-	
 	/*
 	 * Scanner takes in input for player choice
-	 * TODO should wonder be chosen like a card?
 	 */
-	public static void optionSelection()
-	{
-		System.out.println("Type 'Play', 'Build' (wonder), 'Burn', 'Display' (cards and other info)");
-		String input = keyboard.next();
-		
-		switch (input) {
-		case "Play":
-			currentWonder.setAction("Play");
-			handSelection();
-			break;
-		case "Build":
-			currentWonder.setAction("Build");
-			// doThing();
-			break;
-		case "Burn":
-			currentWonder.setAction("Burn");
-			// burn();
-			break;
-		case "display":
-			// display();
-			break;
-		}
-		
-	}
-	
 	public static void handSelection()
 	{
 		int playerInput = 0;
@@ -147,25 +106,32 @@ public class TextRunner
 			System.out.print("Choose index of card to play: ");
 			
 			try {
-			playerInput = keyboard.nextInt();
-			} catch (InputMismatchException e) {
+			playerInput = input.nextInt();
+			cards = currentWonder.getCardsPlayed().get(currentHand.get(playerInput).getColor());
+			cards.add(currentHand.get(playerInput));
+			} 
+			catch (InputMismatchException e) {
 				System.out.println("Error: cannot convert from String to int");
-				break;
+			}
+			catch(IndexOutOfBoundsException e) {
+				if(currentHand.size() == 0)
+				System.out.println("There are no cards left in the hand");
+				else System.out.println("Woah! That number is too big! Please try again");
 			}
 			System.out.println();
+			
+			//System.out.println();
 		}
 		while (playerInput < 0 || playerInput > currentHand.size() - 1);
 		
-		// TODO should this operation be manual and inside TextRunner, or should it be a method in gameState?
+		// TODO should this operation be manual and inside TextRunner?
 		currentWonder.setSelectedCard(currentHand.remove(playerInput));
-		System.out.println("Card \"" + currentWonder.getSelectedCard().getName() + "\" chosen");
-		// TODO setAction? formatting action
+		System.out.println("Chose card " + currentWonder.getSelectedCard().getName());
+		
+		if(currentHand.size() == 1) {
+			endOfRound = true;
+			currentWonder.setMoney(currentWonder.getMoney()+3);
+		}
 	}
 }
-
-
-
-
-
-
 
