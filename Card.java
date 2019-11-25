a049e3caf5d0111f144287ea70103e68f17f1695
malgantual <56133144+malgantual@users.noreply.github.com>
2019-11-25 17:22:44 -0600

    Card - updated toString

--- Card.java
+++ Card.java
@@ -57,7 +57,7 @@ public abstract class Card
 	public String toString()
 	{
 		//return String.format("%-7s %-15s Chain: %-13s Cost: %-25s Players: %-3s", getColor(), getName(), getChain(), getCost(), getNumberOfPlayers());
-		return getName() + "; Chain:" + getChain() + "; Cost:" + getCost() + "; Players:" + getNumberOfPlayers();
+		return getColor() + " " + getName() + "; Chain:" + getChain() + "; Cost:" + getCost() + "; Players:" + getNumberOfPlayers();
 	}
 	
 }
