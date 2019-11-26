16471382c3ad29a545717ba511a1224847ff24af
malgantual <56133144+malgantual@users.noreply.github.com>
2019-11-25 18:23:11 -0600

    test

--- Tester.java
+++ Tester.java
@@ -3,10 +3,11 @@ public class Tester
 {
 	public static void main(String[] args)
 	{
-		GameState state = new GameState();
-		
-		GuildCard c = (GuildCard) state.getDeck().get(2).get(4);
-		//System.out.println((c.getClass() + "").substring(" ", ));
-		//state.getDeck().get(3).add(new Commercial)
+		int HANDXPOS = 25;
+		// draw card images
+		for (int i = 0; i < 7; i++)
+		{
+			System.out.println(190 * i + 25);
+		}
 	}
 }
